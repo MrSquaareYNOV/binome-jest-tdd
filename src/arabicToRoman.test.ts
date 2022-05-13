ebcc804bac454d4bc460ce081814c0e647e95f82
Guillaume Bonnet <mrsquaare@mrsquaare.fr>
2022-05-13 13:57:52 +0200

    test: add tests for empty string cases

--- src/arabicToRoman.test.ts
+++ src/arabicToRoman.test.ts
@@ -4,4 +4,16 @@ describe("arabicToRoman", () => {
   it("should return a string", () => {
     expect(typeof arabicToRomain(1)).toBe("string");
   });
+
+  it("should return empty string for 0", () => {
+    expect(arabicToRomain(0)).toBe("");
+  });
+
+  it("should return empty string for negative number", () => {
+    expect(arabicToRomain(-1)).toBe("");
+  });
+
+  it("should not return empty string for positive number", () => {
+    expect(arabicToRomain(1)).not.toBe("");
+  });
 });
