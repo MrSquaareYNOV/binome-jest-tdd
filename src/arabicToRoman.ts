e4097dddd64e7d18195717778f7d4294c0316889
Guillaume Bonnet <mrsquaare@mrsquaare.fr>
2022-05-13 15:04:05 +0200

    feat: refactor for substractables and repeatables

--- src/arabicToRoman.ts
+++ src/arabicToRoman.ts
@@ -8,15 +8,13 @@ export const arabicToRomain = (nb: number): string => {
   }
 
   const arabicRomanMap = [
-    { arabic: 1, roman: "I" },
-    { arabic: 2, roman: "II" },
-    { arabic: 3, roman: "III" },
-    { arabic: 5, roman: "V" },
-    { arabic: 10, roman: "X" },
-    { arabic: 50, roman: "L" },
-    { arabic: 100, roman: "C" },
-    { arabic: 500, roman: "D" },
-    { arabic: 1000, roman: "M" },
+    { arabic: 1, roman: "I", repeatable: true },
+    { arabic: 5, roman: "V", subtractableBy: "I" },
+    { arabic: 10, roman: "X", repeatable: true, subtractableBy: "I" },
+    { arabic: 50, roman: "L", subtractableBy: "X" },
+    { arabic: 100, roman: "C", repeatable: true, subtractableBy: "X" },
+    { arabic: 500, roman: "D", subtractableBy: "C" },
+    { arabic: 1000, roman: "M", repeatable: true, subtractableBy: "C" },
   ];
 
   const found = arabicRomanMap.find(({ arabic }) => nb === arabic)?.roman;
@@ -25,31 +23,31 @@ export const arabicToRomain = (nb: number): string => {
     return found;
   }
 
-  if (nb + arabicRomanMap[0].arabic === arabicRomanMap[3].arabic) {
-    return arabicRomanMap[0].roman + arabicRomanMap[3].roman;
+  const subtractables = arabicRomanMap.filter(
+    ({ subtractableBy }) => subtractableBy
+  );
+
+  for (const subtractable of subtractables) {
+    const subtractableByMap = arabicRomanMap.find(
+      ({ roman }) => roman === subtractable.subtractableBy
+    );
+
+    if (!subtractableByMap) continue;
+    if (nb !== subtractable.arabic - subtractableByMap.arabic) continue;
+
+    return subtractableByMap.roman + subtractable.roman;
   }
 
-  if (nb + arabicRomanMap[4].arabic === arabicRomanMap[5].arabic) {
-    return arabicRomanMap[4].roman + arabicRomanMap[5].roman;
-  }
+  const repeatables = arabicRomanMap.filter(({ repeatable }) => repeatable);
 
-  if (nb <= 30) {
+  const repeatable = repeatables.find(({ arabic }) => nb <= arabic * 3);
+
+  if (repeatable) {
     let result = "";
 
-    while (nb > 0) {
-      result += "X";
-      nb -= 10;
-    }
-
-    return result;
-  }
-
-  if (nb <= 300) {
-    let result = "";
-
-    while (nb > 0) {
-      result += "C";
-      nb -= 100;
+    while (nb >= repeatable.arabic) {
+      result += repeatable.roman;
+      nb -= repeatable.arabic;
     }
 
     return result;
